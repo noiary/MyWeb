0de100c67720db13308b0a409af32e7088d26366
maodq <noiary@126.com>
2018-12-28 23:35:48 +0800

    optimize

--- home/views.py
+++ home/views.py
@@ -6,5 +6,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse(
-        "<H2>Hi, this is Max and Elsa</H2> <H3>This's our first web-site :)</H3><H3>to be continued...</H3>")
+    return render(request, 'index.html')
