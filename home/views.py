eb56bbd6582288fd50ab667c6c4522f9c8965b83
noiary <noiary@126.com>
2018-12-28 06:56:36 +0000

    modify .gitignore

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<H2>Hi, this is Max and Elsa</H2> <H3>This's our first Web-Home :)</H3><H3>To be continued...</H3>")
+    return HttpResponse("<H2>Hi, this is Max and Elsa</H2> <H3>This's our first web-site :)</H3><H3>to be continued...</H3>")
